a7d47c4ecaa9cb72787711a8201522770be7eda1
segfault0x61 <torresj1997@outlook.com>
2022-05-03 16:05:29 -0500

    creating a gretting command

--- src/listeners/onReady.ts
+++ src/listeners/onReady.ts
@@ -1,7 +1,11 @@
 import { Client } from 'discord.js';
+import { SlashCommands } from '../slash-commands';
 
 export const onReady = (client: Client) => {
   client.on('ready', () => {
     console.log(`${client.user?.username} is online`);
+
+    const commands = SlashCommands.map(c => c.command.toJSON());
+    client.application?.commands.set(commands)
   });
 };
