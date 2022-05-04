94c7d70f0c80b9ec2fff93c30f3ee1ecfe6d7911
segfault0x61 <torresj1997@outlook.com>
2022-05-03 23:36:55 -0500

    user info command

--- src/slash-commands/index.ts
+++ src/slash-commands/index.ts
@@ -2,9 +2,11 @@ import { SlashCommand } from '../types';
 import { AddCommand } from './add';
 import { GoogleCommand } from './google';
 import { HelloCommand } from './hello';
+import { UserInfoCommand } from './userInfo';
 
 export const SlashCommands: SlashCommand[] = [
   HelloCommand,
   AddCommand,
   GoogleCommand,
+  UserInfoCommand
 ];
